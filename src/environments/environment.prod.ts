1ed1f4e48cf0ddef2d924852fc23e5544475bf76
Haoran Yu <haoranyu6@gmail.com>
2020-02-20 02:05:03 -0800

    Deploy on AWS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://nodeangular-env.x7bbmstpzt.us-east-2.elasticbeanstalk.com/api'
+  apiUrl: 'http://nodeangular-env.jnuqhwcvgw.us-east-2.elasticbeanstalk.com/api'
 };
